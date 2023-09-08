63c75d458946fc3582dac9f7bb6f9147dd20d9b3
Victoria <piklad1996@gmail.com>
2023-09-08 17:07:02 -0500

    cmt

---
+++ index.js
@@ -0,0 +1,61 @@
+// initallize prompt
+let userChoice = "";
+
+function getComputerChoice(){
+    const options = ["Rock", "Paper", "Scissors"];
+    // 0, 1, 2 respectively of the array
+    const index = Math.floor(Math.random() * options.length);
+    let choice = options[index]
+
+    //console.log(options[index]); 
+    return choice;
+}
+getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+
+    playerSelection = userChoice; // from Global Variable
+    computerSelection = getComputerChoice();
+
+    // ignore cases
+    let playerChoice = playerSelection.toUpperCase();
+    let computerChoice = computerSelection.toUpperCase();
+
+    // console.log("Player: " +  playerChoice );
+    // console.log("Computer: " +  computerChoice);
+
+    try {
+        if(playerChoice == computerChoice){
+            return "It's a tie!";
+        }else if(playerChoice == "ROCK" && computerChoice == "PAPER"){
+            return "You Lose! Paper beats rock.";
+        }else if(playerChoice == "ROCK" && computerChoice == "SCISSORS"){
+            return "You WIN! Rock beats scissors.";
+        }else if(playerChoice == "PAPER" && computerChoice == "ROCK"){
+            return "You Lose! Rock beats paper.";
+        }else if(playerChoice == "PAPER" && computerChoice == "SCISSORS"){
+            return "You Lose! Scissors beats paper.";
+        }else if(playerChoice == "SCISSORS" && computerChoice == "ROCK"){
+            return "You Lose! Rock beats scissors.";
+        }else if(playerChoice == "SCISSORS" && computerChoice == "PAPER"){
+            return "You Win! Scissors beats paper.";
+        }else{
+            return "Error: Invalid Choice."
+        }
+    } catch (error) {
+        return "Incorrect Input";
+    }
+    
+}
+playRound();
+
+function game(){
+    let i = 0;
+    while(i < 5){
+        // ask more questions till loop ends
+        userChoice = window.prompt("Rock, Paper or Scissors?");
+        alert(playRound());
+        i++;
+    }
+}
+game();
