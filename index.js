d84ac12b7606bece4c899edf896b727dd2326976
Victoria <piklad1996@gmail.com>
2023-09-12 16:54:46 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-// initallize prompt
+
 let userChoice = "";
+let compChoice = "";
+let playerScore = 0;
+
+const btnEls = document.querySelectorAll("button");
+const resultEl = document.querySelector("#results");
+
+btnEls.forEach((b) =>{
+    b.addEventListener('click', ()=>{
+        // console.log(b.id +": clicked");
+        userChoice = b.textContent;
+        compChoice = getComputerChoice();
+        playRound(userChoice, compChoice);
+    });
+});
 
 function getComputerChoice(){
     const options = ["Rock", "Paper", "Scissors"];
@@ -10,52 +24,49 @@ function getComputerChoice(){
     //console.log(options[index]); 
     return choice;
 }
-getComputerChoice();
+//getComputerChoice();
 
 function playRound(playerSelection, computerSelection){
-
-    playerSelection = userChoice; // from Global Variable
-    computerSelection = getComputerChoice();
-
     // ignore cases
-    let playerChoice = playerSelection.toUpperCase();
-    let computerChoice = computerSelection.toUpperCase();
-
-    // console.log("Player: " +  playerChoice );
-    // console.log("Computer: " +  computerChoice);
+    let playerChoice = "";
+    let computerChoice = "";
+    playerChoice = playerSelection.toUpperCase();
+    computerChoice = computerSelection.toUpperCase();
+    
+    console.log("Playround() Player: " +  playerChoice);
+    console.log("Playround() Computer: " +  computerChoice);
 
     try {
         if(playerChoice == computerChoice){
-            return "Computer chose "+ computerChoice + "\n" + "It's a tie!";
+            resultEl.textContent = "Computer chose "+ computerChoice + "\n" + "It's a tie!"
         }else if(playerChoice == "ROCK" && computerChoice == "PAPER"){
-            return "Computer chose "+ computerChoice + "\n" + "You Lose! Paper beats rock.";
+            resultEl.textContent = "Computer chose "+ computerChoice + "\n" + "You Lose! Paper beats rock.";
         }else if(playerChoice == "ROCK" && computerChoice == "SCISSORS"){
-            return "Computer chose "+ computerChoice + "\n" + "You WIN! Rock beats scissors.";
+            resultEl.textContent = "Computer chose "+ computerChoice + "\n" + "You WIN! Rock beats scissors.";
         }else if(playerChoice == "PAPER" && computerChoice == "ROCK"){
-            return "Copmuter chose "+ computerChoice + "\n" + "You Lose! Rock beats paper.";
+            resultEl.textContent = "Copmuter chose "+ computerChoice + "\n" + "You Lose! Rock beats paper.";
         }else if(playerChoice == "PAPER" && computerChoice == "SCISSORS"){
-            return "Computer chose " + computerChoice + "\n" + "You Lose! Scissors beats paper.";
+            resultEl.textContent = "Computer chose " + computerChoice + "\n" + "You Lose! Scissors beats paper.";
         }else if(playerChoice == "SCISSORS" && computerChoice == "ROCK"){
-            return "Computer chose " + computerChoice + "\n" + "You Lose! Rock beats scissors.";
+            resultEl.textContent = "Computer chose " + computerChoice + "\n" + "You Lose! Rock beats scissors.";
         }else if(playerChoice == "SCISSORS" && computerChoice == "PAPER"){
-            return "Computer chose " + computerChoice + "\n" + "You Win! Scissors beats paper.";
+            resultEl.textContent = "Computer chose " + computerChoice + "\n" + "You Win! Scissors beats paper.";
         }else{
-            return "Error: Invalid Choice."
+            resultEl.textContent = "Error";
         }
     } catch (error) {
         return "Incorrect Input";
     }
     
 }
-playRound();
 
 function game(){
-    let i = 0;
-    while(i < 5){
-        // ask more questions till loop ends
-        userChoice = window.prompt("Rock, Paper or Scissors?");
-        alert(playRound());
-        i++;
-    }
+    // let i = 0;
+    // while(i < 5){
+    //     // ask more questions till loop ends
+    //     userChoice = window.prompt("Rock, Paper or Scissors?");
+    //     alert(playRound());
+    //     i++;
+    // }
 }
 game();
